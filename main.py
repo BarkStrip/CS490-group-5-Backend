9644da2a6bc1c32ce182dc701b972f75040707c4
EthanDiaz03 <luke09usa@gmail.com>
2025-11-18 17:49:34 -0500

    dotenv???

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-# load_dotenv()   # only needed locally. 
+load_dotenv()   # only needed locally. 
 from app.config import Config
 from app.extensions import db
 
