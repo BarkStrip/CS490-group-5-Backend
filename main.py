aff9e5200d3bb1f7aad4da9ca0006804f1206165
EthanDiaz03 <luke09usa@gmail.com>
2025-11-18 17:47:49 -0500

    rid of doten

--- main.py
+++ main.py
@@ -14,8 +14,8 @@
 # ----------------------------------------------------------------------------
 from flask import Flask, jsonify
 from flask_cors import CORS
-from dotenv import load_dotenv
-load_dotenv()   # only needed locally. 
+# from dotenv import load_dotenv
+# load_dotenv()   # only needed locally. 
 from app.config import Config
 from app.extensions import db
 
